2f693a910dbaeb2dfb3208f38e092037458dbe9b
oooaaahappyday <joseph.boisselier@gmail.com>
2017-08-16 00:52:55 +0200

    creation class operation

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,41 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app',
-    template: `<h1>{{ name }}</h1>`
+  selector: 'app',
+  template: `
+  	<h1>{{ title }} de {{ user.name }}</h1>
+  		<div><label>Catégorie : </label> {{ operation.categorie }} </div>
+  		<div><label>Commentaire : </label> {{ operation.commentaire }} </div>
+  		<div><label>Destinataire : </label> {{ operation.destinataire }} </div>
+  		<div><label>Emetteur : </label> {{ operation.emetteur }} </div>
+  		<div><label>Montant : </label> {{ operation.montant }} euros</div>
+  `
 })
-export class AppComponent {
-    name: string;
 
-    constructor() {
-        this.name = 'Mes comptes';
-    }
+export class AppComponent {
+	title = 'Comptes';
+	user: User {
+		name: 'Joseph';
+		age : 35;
+	};
+	operation : Operation {
+		categorie: 'salaire',
+		commentaire : 'pas assez',
+		destinataire : 'Joseph',
+		emetteur : 'RnD',
+		montant : 1000
+  };
+}
+
+export class User {
+	name: string;
+	age : number;
+}
+
+export class Operation {
+	categorie   : string;
+	commentaire : string;
+	destinataire: string;
+	emetteur    : string;
+	montant     : number;
 }
